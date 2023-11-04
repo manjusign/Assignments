f364bf9d291e716e2ff484c90fd90bfd2b02d317
manjusign <manjusrimj@gmail.com>
2023-11-04 11:50:57 +0530

    series14 4th and 5th problem added

---
+++ src/series14/Assignment4.java
@@ -0,0 +1,11 @@
+package series14;
+
+public class Assignment4 
+{
+	public static void main(String[] args)
+	{
+		MathOperations m1 = new MathOperations();
+		double result = m1.areaOfCircle(2);
+		System.out.println("Area of Circle is : "+ result);
+	}
+}
---
+++ src/series14/BasicCalculator.java
@@ -0,0 +1,11 @@
+package series14;
+
+public class BasicCalculator implements Calculator
+{
+	public double add(double a, double b) 
+	{
+		double result = a + b;
+		return result;
+	}
+
+}
